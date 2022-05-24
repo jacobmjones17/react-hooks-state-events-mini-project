f870a83bbfaa18f96d9c7245a2210253c808e8a4
Jacob <jacobmjones17@gmail.com>
2022-05-23 23:42:53 -0400

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
-
 import { CATEGORIES, TASKS } from "../data";
-console.log("Here's the data you're working with");
-console.log({ CATEGORIES, TASKS });
 
 function App() {
+  const [allTasks, setAllTasks] = useState(TASKS)
+  const [category, setCategory] = useState("All")
+
+  function handleAddTask(task) {
+    setAllTasks(...allTasks, task)
+  }
+
+  function deleteTask(event) {
+    console.log(event)
+    const filteredTasks = allTasks.filter(task => task.text !== event)
+
+    setAllTasks(filteredTasks)
+  }
+  const categoryFilter = allTasks.filter(
+    (task) => category === "All" || task.category === category
+  )
+
+  console.log(categoryFilter)
+  console.log(allTasks)
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
+      <CategoryFilter categories={CATEGORIES} category={category} setCategory={setCategory}/>
+      <NewTaskForm handleAddTask={handleAddTask} aCategory={CATEGORIES.filter((c) => c !== "All")}/>
+      <TaskList tasks={allTasks} deleteTask={deleteTask} categoryFilter={categoryFilter}/>
     </div>
   );
 }
--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,10 +1,18 @@
 import React from "react";
 
-function CategoryFilter() {
+function CategoryFilter({categories, category, setCategory}) {
+
+  const categoryPicker = categories.map((c) => {
+    const name = c === category ? "selected" : null
+    return (
+      <button key={c} className={name} onClick={() => setCategory(c)}>{c}</button>
+    )
+  })
+
   return (
     <div className="categories">
       <h5>Category filters</h5>
-      {/* render <button> elements for each category here */}
+      {categoryPicker}
     </div>
   );
 }
